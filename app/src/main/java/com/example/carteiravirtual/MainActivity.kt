b67288000d9f0bb68288cbacbed962c838f1a4a4
FellipeZizo <fellipelipearaujo@gmail.com>
2025-06-09 21:17:46 -0300

    tela 2ddoiiiiis

--- app/src/main/java/com/example/carteiravirtual/MainActivity.kt
+++ app/src/main/java/com/example/carteiravirtual/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.carteiravirtual
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,5 +17,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        
+        abrirTelaConversao()
+    }
+    
+    private fun abrirTelaConversao() {
+        val intent = Intent(this, ConvertActivity::class.java)
+        startActivity(intent)
     }
 }
